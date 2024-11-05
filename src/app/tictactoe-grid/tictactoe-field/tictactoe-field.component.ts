54e043f1db6ac331063cf905d69de4627b357760
FriendSheeep39 <v.tea1@students.htl-leonding.ac.at>
2024-11-05 22:13:00 +0100

    Implement tictactoe logic

--- src/app/tictactoe-grid/tictactoe-field/tictactoe-field.component.ts
+++ src/app/tictactoe-grid/tictactoe-field/tictactoe-field.component.ts
@@ -1,4 +1,4 @@
-import {Component, input, InputSignal, output, OutputEmitterRef} from '@angular/core';
+import {Component, input, InputSignal, output, OutputEmitterRef, signal, WritableSignal} from '@angular/core';
 import {Player} from '../tictactoe-grid.component';
 
 @Component({
@@ -11,16 +11,16 @@ import {Player} from '../tictactoe-grid.component';
 
 export class TictactoeFieldComponent {
   player: InputSignal<Player> = input.required();
-  playerOutput: string = "";
+  playerOutput: WritableSignal<string> = signal("");
   fieldIndex: InputSignal<number> = input.required();
   onClick: OutputEmitterRef<number> = output();
 
   protected handleClick() {
     if (this.player() === Player.one || this.player() === Player.none) {
-      this.playerOutput = "x";
+      this.playerOutput.set("x");
     }
     else if (this.player() === Player.two) {
-      this.playerOutput = "o";
+      this.playerOutput.set("o");
     }
     this.onClick.emit(this.fieldIndex());
   }
--- src/app/tictactoe-grid/tictactoe-grid.component.ts
+++ src/app/tictactoe-grid/tictactoe-grid.component.ts
@@ -15,7 +15,7 @@ export class TictactoeGridComponent {
   protected readonly Player = Player;
   public readonly fieldCount: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
   public readonly writeSignal: WritableSignal<string> = signal('');
-  public readonly board: WritableSignal<Player>[] = Array(this.fieldCount.length).fill(signal(this.Player.none));
+  public readonly board: WritableSignal<Player[]> = signal(Array(this.fieldCount.length).fill(Player.none))
 
   public winnerMessage: string = "Oga";
   public currentPlayerMessage = "x";
@@ -25,8 +25,8 @@ export class TictactoeGridComponent {
   public CurrentPlayer: Player = Player.one;
 
   public draw(index: number) {
-    this.board[index].set(this.CurrentPlayer);
-    
+    this.board()[index] = this.CurrentPlayer;
+
     if (this.checkWinner()) {
       this.winner = this.CurrentPlayer;
       this.winnerMessage = `Player ${this.winner} won!`;
@@ -46,7 +46,7 @@ export class TictactoeGridComponent {
   }
 
   public restartGame() {
-    this.board.fill(signal(this.Player.none));
+    this.board.set(Array(this.fieldCount.length).fill(Player.none))
   }
 
   private checkWinner(): boolean {
@@ -56,9 +56,9 @@ export class TictactoeGridComponent {
     for (let row = 0; row < size; row++) {
       const start = row * size;
       if (
-        this.board[start]() !== this.Player.none &&
-        this.board[start]() === this.board[start + 1]() &&
-        this.board[start]() === this.board[start + 2]()
+        this.board()[start] !== this.Player.none &&
+        this.board()[start] === this.board()[start + 1] &&
+        this.board()[start] === this.board()[start + 2]
       ) {
         return true;
       }
@@ -67,9 +67,9 @@ export class TictactoeGridComponent {
     // Check columns
     for (let col = 0; col < size; col++) {
       if (
-        this.board[col]() !== this.Player.none &&
-        this.board[col]() === this.board[col + size]() &&
-        this.board[col]() === this.board[col + 2 * size]()
+        this.board()[col] !== this.Player.none &&
+        this.board()[col] === this.board()[col + size] &&
+        this.board()[col] === this.board()[col + 2 * size]
       ) {
         return true;
       }
@@ -77,18 +77,18 @@ export class TictactoeGridComponent {
 
     // Check diagonal (top-left to bottom-right)
     if (
-      this.board[0]() !== this.Player.none &&
-      this.board[0]() === this.board[4]() &&
-      this.board[0]() === this.board[8]()
+      this.board()[0] !== this.Player.none &&
+      this.board()[0] === this.board()[4] &&
+      this.board()[0] === this.board()[8]
     ) {
       return true;
     }
 
     // Check diagonal (top-right to bottom-left)
     if (
-      this.board[2]() !== this.Player.none &&
-      this.board[2]() === this.board[4]() &&
-      this.board[2]() === this.board[6]()
+      this.board()[2] !== this.Player.none &&
+      this.board()[2] === this.board()[4] &&
+      this.board()[2] === this.board()[6]
     ) {
       return true;
     }
